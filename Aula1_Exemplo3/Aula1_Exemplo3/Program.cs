ced294f52f36cc6b7ac516c8b5046ea9e82774d0
tatianesararoli <tatianesararoli@gmail.com>
2018-02-27 22:18:52 -0300

    first commit

---
+++ Aula1_Exemplo3/Aula1_Exemplo3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aula1_Exemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const double pi = 3.14159265358979323846264338327950;
+
+            double raio = 10;
+            double area;
+
+            Console.WriteLine("Cálculo da área de um Círculo \n");
+            Console.WriteLine("Fórmula: area = pi * raio * raio \n");
+
+            area = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("Área = {0}", area);
+            Console.WriteLine("Área = {0:N}", area);
+            Console.WriteLine("Área = {0:N5}", area);
+
+            Console.ReadKey();
+
+        }
+    }
+}
