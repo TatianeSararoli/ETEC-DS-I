4d8079a2bdb3be00a402e6f51452f75bd01dec97
Tatiane Sararoli <36748975+TatianeSararoli@users.noreply.github.com>
2018-03-27 16:06:53 -0300

    Add files via upload

---
+++ Aula2_Exemplo10/Aula2_Exemplo10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aula2_Exemplo10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            string verdade = "sim", falso = "não";
+
+            Console.Write("Digite um número inteiro: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro número inteiro: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("{0} > {1} ? Resposta: ", a, b);
+            Console.WriteLine(a > b ? verdade : falso);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula2_Exemplo5/Aula2_Exemplo5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aula2_Exemplo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string cidade;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite o nome da Cidade que você nasceu: ");
+            cidade = Console.ReadLine();
+
+            Console.WriteLine("\n***Informações Digitadas*** \n");
+            Console.WriteLine("Seu nome: " + nome);
+            Console.WriteLine("Cidade de nascimento: " + cidade);
+
+            Console.ReadKey();
+        }
+    }
+}
