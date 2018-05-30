b195699ed748a07243d1e106bfe4cbee0e64fc5a
Tatiane Sararoli <36748975+TatianeSararoli@users.noreply.github.com>
2018-05-29 21:32:52 -0300

    Add files via upload

---
+++ Exercicios20180522-Menus/Menus/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Menus
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teste1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teste2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BtnForm = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.BtnValidForm = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.BtnSairForm = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lbldata = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblhora = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivosToolStripMenuItem
+            // 
+            this.arquivosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.teste1ToolStripMenuItem,
+            this.teste2ToolStripMenuItem});
+            this.arquivosToolStripMenuItem.Name = "arquivosToolStripMenuItem";
+            this.arquivosToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.arquivosToolStripMenuItem.Text = "Formulários";
+            // 
+            // teste1ToolStripMenuItem
+            // 
+            this.teste1ToolStripMenuItem.Name = "teste1ToolStripMenuItem";
+            this.teste1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.teste1ToolStripMenuItem.Text = "Teste1";
+            this.teste1ToolStripMenuItem.Click += new System.EventHandler(this.teste1ToolStripMenuItem_Click);
+            // 
+            // teste2ToolStripMenuItem
+            // 
+            this.teste2ToolStripMenuItem.Name = "teste2ToolStripMenuItem";
+            this.teste2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.teste2ToolStripMenuItem.Text = "Teste2";
+            this.teste2ToolStripMenuItem.Click += new System.EventHandler(this.teste2ToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BtnForm,
+            this.toolStripSeparator1,
+            this.BtnValidForm,
+            this.toolStripSeparator2,
+            this.BtnSairForm});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BtnForm
+            // 
+            this.BtnForm.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BtnForm.Image = ((System.Drawing.Image)(resources.GetObject("BtnForm.Image")));
+            this.BtnForm.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnForm.Name = "BtnForm";
+            this.BtnForm.Size = new System.Drawing.Size(23, 22);
+            this.BtnForm.Text = "toolStripButton1";
+            this.BtnForm.ToolTipText = "Preencher Formulário";
+            this.BtnForm.Click += new System.EventHandler(this.BtnForm_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // BtnValidForm
+            // 
+            this.BtnValidForm.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BtnValidForm.Image = ((System.Drawing.Image)(resources.GetObject("BtnValidForm.Image")));
+            this.BtnValidForm.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnValidForm.Name = "BtnValidForm";
+            this.BtnValidForm.Size = new System.Drawing.Size(23, 22);
+            this.BtnValidForm.Text = "toolStripButton2";
+            this.BtnValidForm.ToolTipText = "Validar Formulário";
+            this.BtnValidForm.Click += new System.EventHandler(this.BtnValidForm_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // BtnSairForm
+            // 
+            this.BtnSairForm.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BtnSairForm.Image = ((System.Drawing.Image)(resources.GetObject("BtnSairForm.Image")));
+            this.BtnSairForm.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnSairForm.Name = "BtnSairForm";
+            this.BtnSairForm.Size = new System.Drawing.Size(23, 22);
+            this.BtnSairForm.Text = "toolStripButton3";
+            this.BtnSairForm.ToolTipText = "Sair do Formulário";
+            this.BtnSairForm.Click += new System.EventHandler(this.BtnSairForm_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lbldata,
+            this.lblhora});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lbldata
+            // 
+            this.lbldata.Name = "lbldata";
+            this.lbldata.Size = new System.Drawing.Size(34, 17);
+            this.lbldata.Text = "Data:";
+            // 
+            // lblhora
+            // 
+            this.lblhora.Name = "lblhora";
+            this.lblhora.Size = new System.Drawing.Size(36, 17);
+            this.lblhora.Text = "Hora:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMenu";
+            this.Text = "Menu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmMenu_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teste1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teste2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton BtnForm;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton BtnValidForm;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton BtnSairForm;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lbldata;
+        private System.Windows.Forms.ToolStripStatusLabel lblhora;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
